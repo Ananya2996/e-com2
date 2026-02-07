a01966962aa55caf6e26d0821cb149123a1ec43e
Ananya2996 <ananyasingh70600@gmail.com>
2026-02-07 17:21:09 +0530

    Add files via upload

---
+++ FE/src/App.js
@@ -0,0 +1,56 @@
+import "./App.css";
+import Navbar from "./Components/Navbar/Navbar";
+import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import Shop from "./Pages/Shop";
+import ShopCategory from "./Pages/ShopCategory";
+import Product from "./Pages/Product";
+import Cart from "./Pages/Cart";
+import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Components/Assets/Frontend_Assets/banner_mens.png";
+import women_banner from "./Components/Assets/Frontend_Assets/banner_women.png";
+import kids_banner from "./Components/Assets/Frontend_Assets/banner_kids.png";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <AppLayout />
+    </BrowserRouter>
+  );
+}
+
+function AppLayout() {
+  const location = useLocation();
+
+  const hideFooter =
+    location.pathname === "/login" || location.pathname.startsWith("/product");
+
+  return (
+    <div>
+      <Navbar />
+
+      <Routes>
+        <Route path="/" element={<Shop />} />
+        <Route
+          path="/men"
+          element={<ShopCategory banner={men_banner} category="men" />}
+        />
+        <Route
+          path="/women"
+          element={<ShopCategory banner={women_banner} category="women" />}
+        />
+        <Route
+          path="/kids"
+          element={<ShopCategory banner={kids_banner} category="kids" />}
+        />
+        <Route path="/product/:productId" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route path="/login" element={<LoginSignup />} />
+      </Routes>
+
+      {!hideFooter && <Footer />}
+    </div>
+  );
+}
+
+export default App;
