51dc5965188617696580a36f6bce1d542148633b
Ananya2996 <ananyasingh70600@gmail.com>
2026-02-07 17:19:10 +0530

    Add files via upload

---
+++ FE/src/Components/Descriptionbox/Descriptionbox.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import './Descriptionbox.css'
+
+const Descriptionbox = () => {
+  return (
+    < div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="description-nav-box">
+            Description
+        </div>
+          <div className="description-nav-box fade">
+          Reviews(120)
+        </div>
+        </div>
+        <div className="descriptionbox-description">
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Mollitia facere nostrum eius reiciendis temporibus aliquam eaque est corporis? Consequuntur, excepturi, officia, eveniet voluptate sequi possimus autem asperiores dolorum iusto expedita sed esse</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iste ipsa hic voluptatem atque ab aliquam. Modi corrupti fuga, totam dolorem iure praesentium voluptatem distinctio quasi. Ad sunt magni voluptatem nostrum?</p>
+        </div>
+      </div>
+      
+    
+  )
+}
+  
+export default Descriptionbox
